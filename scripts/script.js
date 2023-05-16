d244c36f31ca84afecb5159073d36dcf614947ad
elecinas <estherlecina@gmail.com>
2023-05-16 10:11:34 +0200

    crea la home con todas sus secciones excepto thumbnails slider y footer

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,46 @@
-const carousel = document.querySelector('.carousel-inner');
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
+/* CARGA DE IMÁGENES SEGÚN TAMAÑO DE VENTANA */
+
+putImageBySizeScreen();
+window.addEventListener("resize", putImageBySizeScreen);
+
+function putImageBySizeScreen(){
+  const heroCarouselImage1 = document.getElementById("imgCarousel1");
+  const heroCarouselImage2 = document.getElementById("imgCarousel2");
+  const screenWidth = window.innerWidth;
+
+  if (screenWidth < 768) {
+    heroCarouselImage1.src = "./images/hero_img_ramon_xs.jpg";
+    heroCarouselImage2.src = "./images/hero_img_cucumbers_xs.jpg";
+  }  else {
+    heroCarouselImage1.src = "./images/hero_img_ramon_xl.jpg";
+    heroCarouselImage2.src = "./images/hero_img_cucumbers_xl.jpg";
+  }
+}
+
+/* CÓDIGO MENÚ */
+
+const menuPrincipal = document.getElementById("menu");
+
+function showMenu() {
+  menuPrincipal.classList.contains("shown")
+    ? menuPrincipal.classList.remove("shown")
+    : menuPrincipal.classList.add("shown");
+}
+
+/* CÓDIGO CAROUSEL */
+
+const carousel = document.querySelector(".carousel-inner");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
 
 let currentIndex = 0;
-const carouselItems = document.querySelectorAll('.carousel-item');
-carouselItems[currentIndex].classList.add('active');
+const carouselItems = document.querySelectorAll(".carousel-item");
+carouselItems[currentIndex].classList.add("active");
 
 function goToSlide(index) {
   carousel.style.transform = `translateX(-${index * 100}%)`;
-  carouselItems[currentIndex].classList.remove('active');
-  carouselItems[index].classList.add('active');
+  carouselItems[currentIndex].classList.remove("active");
+  carouselItems[index].classList.add("active");
   currentIndex = index;
 }
 
@@ -29,7 +60,7 @@ function goToPrevSlide() {
   }
 }
 
-nextBtn.addEventListener('click', goToNextSlide);
-prevBtn.addEventListener('click', goToPrevSlide);
+nextBtn.addEventListener("click", goToNextSlide);
+prevBtn.addEventListener("click", goToPrevSlide);
 
 // setInterval(goToNextSlide, 3000);
